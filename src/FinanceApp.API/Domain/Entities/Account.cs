ab552fdcfac9d7c9ac8ad9b62404357c3488d4ad
Yasin Coskun <37144063+YasinCoskun35@users.noreply.github.com>
2024-01-27 12:57:58 +0300

    Entities are created

---
+++ src/FinanceApp.API/Domain/Entities/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceApp.API.Domain.Entities
+{
+    public class Account
+    {
+        public Guid UUID { get; set; }
+        public Customer Customer { get; set; }
+        public string Alias { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ src/FinanceApp.API/Domain/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.AccessControl;
+
+namespace FinanceApp.API.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid UUID { get; set; }
+        public string IdNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public List<Account> Accounts { get; set; }
+    }
+}
